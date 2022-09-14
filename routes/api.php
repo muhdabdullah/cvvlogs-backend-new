8a8eae66e921a57181805bc5d376fa7b806fb1a6
mohsinahmad86 <AkDTEvtSmNrf9ejQ5ySy>
2022-09-15 02:31:04 +0500

    passport

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['auth:api']], function () {
+    Route::get('/users', function (){
+        dd(\App\Models\User::all());
+    })->name('users');
+});

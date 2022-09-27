059dd3b69bdd2383ccea51ca76ed55866f513a7d
Mohsin Ahmad <mohsin.ahmad08@hotmail.com>
2022-09-27 22:37:39 +0500

    init

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(['middleware' => ['auth:api']], function () {
+Route::group(['middleware' => ['checkAuth']], function () {
     Route::get('/users', function (){
         dd(\App\Models\User::all());
     })->name('users');

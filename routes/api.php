85bf1e21f6f3b537d4caf8758ef2e825112baab4
Mohsin Ahmad <mohsin.ahmad08@hotmail.com>
2022-10-27 01:14:44 +0500

    #testing CORS issue

--- routes/api.php
+++ routes/api.php
@@ -15,26 +15,24 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::middleware(['cors'])->group(function () {
-    Route::group(['prefix' => '/api.v.1/admin'], function () {
-        Route::post('login', [AuthApiController::class, 'login'])->name('admin.login');
-    });
+Route::group(['prefix' => '/api.v.1/admin'], function () {
+    Route::post('login', [AuthApiController::class, 'login'])->name('admin.login');
+});
 
-    Route::group(['prefix' => '/api.v.1/admin', 'middleware' => ['permission']], function () {
-        Route::get('get-job-list', [DashboardApiController::class, 'getAllJobs'])->name('admin.getAllJobs');
-        Route::get('get-stats', [DashboardApiController::class, 'getStats'])->name('admin.getStats');
-        Route::get('get-monthly-stats', [DashboardApiController::class, 'getMonthlyStats'])->name('admin.getStats');
-        Route::get('get-user-videos', [DashboardApiController::class, 'getUserVideos'])->name('admin.getUserVideos');
-        Route::put('update-video-status', [DashboardApiController::class, 'updateVideoStatus'])->name('admin.updateVideoStatus');
-    });
+Route::group(['prefix' => '/api.v.1/admin', 'middleware' => ['permission']], function () {
+    Route::get('get-job-list', [DashboardApiController::class, 'getAllJobs'])->name('admin.getAllJobs');
+    Route::get('get-stats', [DashboardApiController::class, 'getStats'])->name('admin.getStats');
+    Route::get('get-monthly-stats', [DashboardApiController::class, 'getMonthlyStats'])->name('admin.getStats');
+    Route::get('get-user-videos', [DashboardApiController::class, 'getUserVideos'])->name('admin.getUserVideos');
+    Route::put('update-video-status', [DashboardApiController::class, 'updateVideoStatus'])->name('admin.updateVideoStatus');
+});
 
-    Route::group(['prefix' => '/api.v.2/recruiter', 'middleware' => ['checkAuth']], function () {
-        Route::get('recruiters', [Sample::class, 'index'])->name('recruiters');
-    });
+Route::group(['prefix' => '/api.v.2/recruiter', 'middleware' => ['checkAuth']], function () {
+    Route::get('recruiters', [Sample::class, 'index'])->name('recruiters');
+});
 
-    Route::group(['prefix' => '/api.v.1/user', 'middleware' => ['checkAuth']], function () {
-        Route::get('users', function (){
-            dd(\App\Models\User::all());
-        })->name('users');
-    });
+Route::group(['prefix' => '/api.v.1/user', 'middleware' => ['checkAuth']], function () {
+    Route::get('users', function (){
+        dd(\App\Models\User::all());
+    })->name('users');
 });

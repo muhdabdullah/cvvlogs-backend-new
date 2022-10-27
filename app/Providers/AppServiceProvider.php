7950b562c4b65761a5f6df63519435df794f610c
Mohsin Ahmad <mohsin.ahmad08@hotmail.com>
2022-10-28 01:52:04 +0500

    #testing CORS issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
     }
 }

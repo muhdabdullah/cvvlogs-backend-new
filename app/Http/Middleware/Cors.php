5ff1f0c76a57dcb1cb5c27f00565d5e78c499cd7
Mohsin Ahmad <mohsin.ahmad08@hotmail.com>
2022-10-28 01:05:14 +0500

    #testing CORS issue

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,20 +16,10 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        header("Access-Control-Allow-Origin: *");
-
-        $headers = [
-            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization'
-        ];
-        if ($request->getMethod() == "OPTIONS") {
-            return response('OK')
-                ->withHeaders($headers);
-        }
-
-        $response = $next($request);
-        foreach ($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
+            ->header('Access-Control-Allow-Credentials',' true');
     }
 }
--- config/cors.php
+++ config/cors.php
@@ -17,10 +17,10 @@ return [
 
     'paths'                    => ['*', 'api/*'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['*', '*'],
+    'allowed_origins'          => ['*', 'http://dev.projectcamp.io/*'],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*', '*'],
     'exposed_headers'          => [],
-    'max_age'                  => 0,
+    'max_age'                  => false,
     'supports_credentials'     => false,
 ];

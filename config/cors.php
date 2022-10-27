9cf5395de0ebd2eb7653295ee0272713fb7d044f
Mohsin Ahmad <mohsin.ahmad08@hotmail.com>
2022-10-28 01:48:03 +0500

    #testing CORS issue

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths'                    => ['*'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['http://dev.projectcamp.io'],
+    'allowed_origins'          => ['*'],
     'allowed_origins_patterns' => ['*'],
     'allowed_headers'          => ['*', '*'],
     'exposed_headers'          => [],

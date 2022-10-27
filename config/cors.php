ac05c6a7af0e11c25fdd76723ab75cae9a4774ad
Mohsin Ahmad <mohsin.ahmad08@hotmail.com>
2022-10-28 00:57:26 +0500

    #testing CORS issue

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths'                    => ['*'],
+    'paths'                    => ['*', 'api/*'],
     'allowed_methods'          => ['*'],
     'allowed_origins'          => ['*', '*'],
     'allowed_origins_patterns' => [],
